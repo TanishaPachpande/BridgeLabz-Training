b6dceccdc54c6bf18af36e786143d7dd832374f9
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-17 16:49:40 +0530

    [Tanisha Pachpande] Add. Added FindAgeOfHarry.java file

---
+++ gcr-codebase/programming-elements/Level 1/FindAgeOfHarry.java
@@ -0,0 +1,14 @@
+public class FindAgeOfHarry{
+  public static void main(String[] args){
+
+       // taking Birth and current year as input.
+       int birthYear = 2000;
+       int currentYear = 2024;
+
+       // Calculating the current age.
+       int currentAgeOfHarry = currentYear-birthYear;
+
+       // Display the result.
+       System.out.println("Harry's age in 2024 is "+ currentAgeOfHarry);
+  }
+}
