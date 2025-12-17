a89c9ba5bf9b2b1b56d489b750339733ec169f02
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-17 16:46:16 +0530

    [Tanisha Pachpande] Add. Added ConvertKmsToMiles.java file

---
+++ gcr-codebase/programming-elements/Level 1/ConvertKmsToMiles.java
@@ -0,0 +1,13 @@
+public class ConvertKmsToMiles{
+  public static void main(String[] args){
+
+      // Assigned input.
+      double distanceInKms = 10.8;
+
+      // Converting kilometers into miles.
+      double distanceInMiles = 1.6 * distanceInKms;
+
+      // Display the result.
+      System.out.println("The distance "+ distanceInKms+" km in miles is "+ distanceInMiles);
+  }
+}
