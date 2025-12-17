e5b82200d442d8fe511cdc056df2a007a54bff6f
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-17 16:40:46 +0530

    [Tanisha Pachpande] Add. Added AreaOfTriangle.java file

---
+++ gcr-codebase/programming-elements/Level 1/AreaOfTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle{
+  public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+
+        // Taking user input of base and height.
+        int base = input.nextInt();
+        int height = input.nextInt();
+
+        // Calculating area in cms and inches.
+        double areaInCm= 0.5 * base * height;
+        double areaInInches= areaInCm / (2.54*2.54);
+
+        // Display the result
+        System.out.println("The Area of Triangle in square inches is "+ areaInInches);
+
+        
+  }
+}
