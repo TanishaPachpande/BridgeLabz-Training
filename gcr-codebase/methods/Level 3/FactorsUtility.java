7134bef2999ef8dde8974d4dc87d8d87abcc213c
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-22 00:59:52 +0530

    [Tanisha Pachpande] Add. Added FactorsUtility.java file

---
+++ gcr-codebase/methods/Level 3/FactorsUtility.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class FactorsUtility {
+
+    // Method to find factors
+    static int[] findFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++)
+            if (number % i == 0) count++;
+
+        int[] factors = new int[count];
+        int index = 0;
+
+        for (int i = 1; i <= number; i++)
+            if (number % i == 0) factors[index++] = i;
+
+        return factors;
+    }
+
+    // Method to find greatest factor
+    static int greatestFactor(int[] factors) {
+        return factors[factors.length - 1];
+    }
+
+    // Method to find sum of factors
+    static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f;
+        return sum;
+    }
+
+    // Method to product of factors
+    static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int f : factors) product *= f;
+        return product;
+    }
+
+    // Method to find product of cube of factors
+    static double productOfCubeOfFactors(int[] factors) {
+        double product = 1;
+        for (int f : factors)
+            product *= Math.pow(f, 3);
+        return product;
+    }
+
+    public static void main(String[] args) {
+        
+        // Input the number
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] factors = findFactors(number);
+
+	// Display the result
+        System.out.println("Greatest Factor: " + greatestFactor(factors));
+        System.out.println("Sum of Factors: " + sumOfFactors(factors));
+        System.out.println("Product of Factors: " + productOfFactors(factors));
+        System.out.println("Product of Cube of Factors: " + productOfCubeOfFactors(factors));
+    }
+}
