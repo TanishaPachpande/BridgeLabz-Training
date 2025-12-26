3137d8edf2f9aefe14b017089ce12580576e9321
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-26 22:29:58 +0530

    [Tanisha Pachpande] Add: Added Employee.java file

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobjects/level1/Employee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobjects/level1/Employee.java
@@ -2,9 +2,28 @@ package com.classandobjects.level1;
 
 public class Employee {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // non-static variables
+    String name;
+    int id;
+    double salary;
 
-	}
+    // Constructor with parameters
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
 
+    // method to display employee details
+    public void displayEmployee() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee id: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+    	// Creating object of a class
+        Employee emp = new Employee("Rohan", 1, 500000);
+        emp.displayEmployee();
+    }
 }
