e603f780555598191671be222dfebf350f0e8504
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-26 13:08:55 +0530

    [Tanisha Pachpande] Add. Added DateComparison.java

---
+++ gcr-codebase/extras/dateAndTime/DateComparison.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DateComparison {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+	// Taking two dates as input and parsing it into date format
+        System.out.print("Enter first date (yyyy-mm-dd): ");
+        LocalDate date1 = LocalDate.parse(sc.nextLine());
+
+        System.out.print("Enter second date (yyyy-mm-dd): ");
+        LocalDate date2 = LocalDate.parse(sc.nextLine());
+
+	// Display the first date and second date in order 
+        if (date1.isBefore(date2)) {
+            System.out.println("First date is BEFORE second date");
+        } 
+        else if (date1.isAfter(date2)) {
+            System.out.println("First date is AFTER second date");
+        } 
+        else {
+            System.out.println("Both dates are SAME");
+        }
+    }
+}
