57e77b40155d7f019a1d90c37eb6341bc2944a23
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-28 01:03:33 +0530

    [Tanisha Pachpande] Add: Added PostgraduateStudent.java file

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/PostgraduateStudent.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/PostgraduateStudent.java
@@ -17,7 +17,7 @@ public class PostgraduateStudent extends Student {
     }
 
     // MAIN METHOD HERE
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         PostgraduateStudent pg = new PostgraduateStudent(
                 101,
