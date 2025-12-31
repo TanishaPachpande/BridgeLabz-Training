9803b026b07f01533831b10db59c6592686f3b1f
TanishaPachpande <tanishapachpande86@gmail.com>
2025-12-31 16:09:57 +0530

    [Tanisha Pachpande] Add: Added DigitalWatchSimulation.java file

---
+++ scenario-based/DigitalWatchSimulation.java
@@ -0,0 +1,16 @@
+public class DigitalWatchSimulation {
+    public static void main(String[] args) {
+
+        for (int hour = 0; hour < 24; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
+
+                if (hour == 13 && minute == 0) {
+                    System.out.println("Power cut. Watch Stopped!!");
+                    return; // stops both loops
+                }
+
+                System.out.printf("%02d:%02d%n", hour, minute);
+            }
+        }
+    }
+}
