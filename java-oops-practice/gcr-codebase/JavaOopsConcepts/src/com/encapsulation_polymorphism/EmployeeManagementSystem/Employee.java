51d2e7e02fa2390d3dd5ecf50151ddf04c7b9012
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-01 23:14:25 +0530

    [Tanisha Pachpande] Add: Added Employee.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism/EmployeeManagementSystem/Employee.java
@@ -0,0 +1,64 @@
+package com.encapsulation_polymorphism.EmployeeManagementSystem;
+
+public abstract class Employee implements Department {
+
+    // Encapsulated fields
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+    private String department;
+
+    // Constructor
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    // Abstract method
+    public abstract double calculateSalary();
+
+    // Concrete method
+    public void displayDetails() {
+        System.out.println("Employee ID : " + employeeId);
+        System.out.println("Name        : " + name);
+        System.out.println("Department  : " + department);
+        System.out.println("Salary      : " + calculateSalary());
+        System.out.println("-----------------------------------");
+    }
+
+    // Getters and Setters (Encapsulation)
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    // Interface method implementations
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
+
