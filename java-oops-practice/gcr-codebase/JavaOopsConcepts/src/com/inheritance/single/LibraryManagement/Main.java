68358b8f88c2c38003b5dd4b8bdcda0d0d0a8812
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-01 12:08:07 +0530

    [Tanisha Pachpande] Add: Added Main.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/single/LibraryManagement/Main.java
@@ -0,0 +1,10 @@
+package com.inheritance.single.LibraryManagement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Author b = new Author("xyz_title", 2025, "xyz_name", "xyz_bio");
+		b.displayInfo();
+	}
+
+}
