250cedddb846255f806604bf36fbf50b151d2694
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-01 11:40:37 +0530

    [Tanisha Pachpande] Add: Added Animal.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/AnimalHierarchy/Animal.java
@@ -0,0 +1,13 @@
+package com.inheritance.hierarchical.AnimalHierarchy;
+
+public class Animal {
+	
+	// Attributes
+	String name;
+	int age;
+	
+	// Method
+	void makeSound() {
+		System.out.println("Animal sound!");
+	}
+}
