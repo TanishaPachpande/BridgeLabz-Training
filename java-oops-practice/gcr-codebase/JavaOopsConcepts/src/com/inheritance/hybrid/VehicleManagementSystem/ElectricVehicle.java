cdd57e5599b62dd566f08fe6b12c6ccb6b62f469
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-01 11:57:28 +0530

    [Tanisha Pachpande] Add: Added ElectricVehicle.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybrid/VehicleManagementSystem/ElectricVehicle.java
@@ -0,0 +1,11 @@
+package com.inheritance.hybrid.VehicleManagementSystem;
+
+public class ElectricVehicle extends Vehicle{
+	ElectricVehicle(String model, int maxSpeed) {
+        super(model, maxSpeed);
+    }
+
+    void charge() {
+        System.out.println("Charging the electric vehicle...");
+    }
+}
