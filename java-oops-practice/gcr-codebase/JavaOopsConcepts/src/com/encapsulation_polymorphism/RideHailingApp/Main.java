37f90f1b93b6cb4bc0378d5da60ed7d2e8c5588b
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-01 23:25:39 +0530

    [Tanisha Pachpande] Add: Added Main.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism/RideHailingApp/Main.java
@@ -0,0 +1,24 @@
+package com.encapsulation_polymorphism.RideHailingApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Vehicle c = new Car(1, "xyz", 5);
+		Vehicle b = new Bike(1, "abc", 2);
+		Vehicle a = new Auto(3, "jkl", 10);
+		
+		c.getVehicleDetails();
+		c.calculateFare(10);
+		System.out.println("------------------------------------");
+		b.getVehicleDetails();
+		b.calculateFare(12);
+		System.out.println("------------------------------------");
+		a.getVehicleDetails();
+		a.calculateFare(15);
+		System.out.println("------------------------------------");
+		
+		c.updateLocation("bhopal");
+		System.out.println(c.getCurrentLocation());
+	}
+
+}
