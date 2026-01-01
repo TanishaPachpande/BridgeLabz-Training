24f97923d5ae174f6f396f7a485e2e13c3a608c8
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-01 23:15:09 +0530

    [Tanisha Pachpande] Add: Added PartTimeEmployee.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism/EmployeeManagementSystem/PartTimeEmployee.java
@@ -0,0 +1,17 @@
+package com.encapsulation_polymorphism.EmployeeManagementSystem;
+
+public class PartTimeEmployee extends Employee {
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(int employeeId, String name, double hourlyRate, int hoursWorked) {
+        super(employeeId, name, 0);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+}
