5798d59ce97704e6e0506dc010020dc004c90321
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-01 23:12:20 +0530

    [Tanisha Pachpande] Add: Added Groceries.java file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism/EcommercePlatform/Groceries.java
@@ -0,0 +1,13 @@
+package com.encapsulation_polymorphism.EcommercePlatform;
+
+public class Groceries extends Product {
+
+    public Groceries(int productId, String name, double price) {
+        super(productId, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.05; // 5% discount
+    }
+}
+
