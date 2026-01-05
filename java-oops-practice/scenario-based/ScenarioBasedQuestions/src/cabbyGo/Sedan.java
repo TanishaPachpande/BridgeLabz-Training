e95c9571da155904e85ff1bc760bf5f7811eada7
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-05 20:25:27 +0530

    [Tanisha Pachpande] Modify: modified Sedan.java file

--- java-oops-practice/scenario-based/ScenarioBasedQuestions/src/cabbyGo/Sedan.java
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/cabbyGo/Sedan.java
@@ -1,28 +1,8 @@
-package cabbyGo;
+package cabbygo;
 
-public class Driver {
-	private String name;
-	private int licenseNumber;
-	public int rating;
-	
-	public String getName() {
-		return name;
-	}
-
-	public int getLicenseNumber() {
-		return licenseNumber;
-	}
-	
-	public Driver(String name, int licenseNumber, int rating) {
-		super();
-		this.name = name;
-		this.licenseNumber = licenseNumber;
-		this.rating = rating;
-	}
-	
-	public void displayDriverDetails() {
-		System.out.println("Driver Name: "+ name);
-		System.out.println("License Number: "+ licenseNumber);
-		System.out.println("Rating: "+ rating);
-	}
+public class Sedan extends Vehicle {
+    public Sedan(String vehicleNumber) {
+        super(vehicleNumber, 4, "Sedan", 15);
+    }
 }
+
