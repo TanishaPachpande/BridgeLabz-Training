1ff6b71f7a17ce6a3524cc2b3c958a085f963628
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-05 20:25:11 +0530

    [Tanisha Pachpande] Modify: modified SUV.java file

--- java-oops-practice/scenario-based/ScenarioBasedQuestions/src/cabbyGo/SUV.java
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/cabbyGo/SUV.java
@@ -1,21 +1,8 @@
-package cabbyGo;
+package cabbygo;
 
-public class Main {
-	public static void main(String[] args) {
-		Vehicle v1 = new Vehicle(1, 120, "SUV");
-		Vehicle v2 = new Vehicle(2, 100, "Sedan");
-		
-		Driver d = new Driver("amit", 123, 5);
-		
-		Fare f = new Fare(50, 100);
-		
-		System.out.println("--------------------------------");
-		v1.displayVehicleDetails();
-		System.out.println("--------------------------------");
-		v2.displayVehicleDetails();
-		System.out.println("--------------------------------");
-		d.displayDriverDetails();
-		System.out.println("--------------------------------");
-		f.calculateFare();
-	}
+public class SUV extends Vehicle {
+    public SUV(String vehicleNumber) {
+        super(vehicleNumber, 6, "SUV", 20);
+    }
 }
+
