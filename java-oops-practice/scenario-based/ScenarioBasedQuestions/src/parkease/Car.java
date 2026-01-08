88828ce059670fa316de7591f709dfaba36f2cf3
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-08 07:35:50 +0530

    [Tanisha Pachpande] Add: Added Car.java file in parkease package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/parkease/Car.java
@@ -0,0 +1,16 @@
+package parkease;
+
+public class Car extends Vehicle {
+
+    public Car(String vehicleNumber) {
+        super(vehicleNumber);
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        double baseRate = 50; // per hour
+        double penalty = hours > 5 ? 100 : 0;
+        return (baseRate * hours) + penalty;
+    }
+}
+
