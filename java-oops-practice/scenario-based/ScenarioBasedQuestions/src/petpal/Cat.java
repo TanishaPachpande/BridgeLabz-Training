3e1bd60c8ca40b0f1a5db7776760121519bfdf6f
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-08 07:38:13 +0530

    [Tanisha Pachpande] Add: Added Cat.java file in petpal package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/petpal/Cat.java
@@ -0,0 +1,13 @@
+package petpal;
+
+public class Cat extends Pet {
+
+    public Cat(String name) {
+        super(name, "Cat");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Cat says: Meow!");
+    }
+}
