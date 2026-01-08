4b40816d0d63ebf2e5f460c593209770ea79ad44
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-08 07:31:55 +0530

    [Tanisha Pachpande] Add: Added User.java file in eventease package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/eventease/User.java
@@ -0,0 +1,17 @@
+package eventease;
+
+public class User {
+
+    private String name;
+    private String email;   // sensitive data (encapsulation)
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
