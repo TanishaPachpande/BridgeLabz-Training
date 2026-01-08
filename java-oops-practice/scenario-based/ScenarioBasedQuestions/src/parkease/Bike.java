2cfe063e789987255189d508aa3d52d48e8fad21
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-08 07:35:38 +0530

    [Tanisha Pachpande] Add: Added Bike.java file in parkease package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/parkease/Bike.java
@@ -0,0 +1,16 @@
+package parkease;
+
+public class Bike extends Vehicle {
+
+    public Bike(String vehicleNumber) {
+        super(vehicleNumber);
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        double baseRate = 20;
+        double penalty = hours > 5 ? 50 : 0;
+        return (baseRate * hours) + penalty;
+    }
+}
+
