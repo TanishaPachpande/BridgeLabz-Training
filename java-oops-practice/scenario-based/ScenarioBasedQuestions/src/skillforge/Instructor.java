4b8919aea25c55124265fbbce24ee235d2943014
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-10 12:04:38 +0530

    [Tanisha Pachpande] Add: Added Instructor.java file in skillforge package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/skillforge/Instructor.java
@@ -0,0 +1,14 @@
+package skillforge;
+
+
+
+public class Instructor extends User {
+
+	public Instructor(String name, String email) {
+	        super(name, email);
+	}
+	
+	public void uploadCourse(String courseName) {
+        System.out.println("Instructor " + name + " uploaded course: " + courseName);
+    }
+}
