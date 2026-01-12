ef8f0fa07741cde694cf5f7d653001507d3115f4
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-12 20:54:32 +0530

    [Tanisha Pachpande] Add: Added Vehicle.java file trafficmanager package

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/com/trafficmanager/Vehicle.java
@@ -0,0 +1,11 @@
+package com.trafficmanager;
+
+public class Vehicle {
+	String vehicleNumber;
+    Vehicle next;
+
+    Vehicle(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+        this.next = null;
+    }
+}
