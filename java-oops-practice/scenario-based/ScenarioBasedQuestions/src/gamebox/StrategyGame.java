ac3b2cfb1c633ecc41f019a761bbd4b8a04ef844
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-15 12:09:21 +0530

    [Tanisha Pachpande] Add: Added StrategyGame.java file in gamebox package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/gamebox/StrategyGame.java
@@ -0,0 +1,13 @@
+package gamebox;
+public class StrategyGame extends Game {
+
+    public StrategyGame(String title, double price, double rating) {
+        super(title, "Strategy", price, rating);
+    }
+
+    @Override
+    public void playDemo() {
+        System.out.println("Playing strategic tutorial demo of " + title);
+    }
+}
+
