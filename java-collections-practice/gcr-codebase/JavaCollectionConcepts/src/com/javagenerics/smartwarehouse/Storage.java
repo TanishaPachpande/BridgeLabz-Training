1221d9ece4bfe28738ab1c7f429ab7120d8c4608
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-15 13:27:07 +0530

    [Tanisha Pacpande] Add: Added Storage.java file in smartwarehouse package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/javagenerics/smartwarehouse/Storage.java
@@ -0,0 +1,18 @@
+package com.javagenerics.smartwarehouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Storage<T extends WarehouseItem> {
+
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
+
