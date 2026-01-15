89d8b8035f86d5a88421ff9a160dda91583afd4b
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-15 12:01:36 +0530

    [Tanisha Pachpande] Add: Added DeveloperInfo.java file in birdsanctuary package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/birdsanctuary/DeveloperInfo.java
@@ -0,0 +1,10 @@
+package birdsanctuary;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface DeveloperInfo {
+    String developer();
+    String version();
+}
