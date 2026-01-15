af30e52a48bc09f486a8bd24e5bd8e6689e27182
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-15 12:07:03 +0530

    [Tanisha Pachpande] Add: Added Transaction.java file in budgetwise package

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/budgetwise/Transaction.java
@@ -0,0 +1,31 @@
+package budgetwise;
+
+import java.time.LocalDate;
+
+public class Transaction {
+
+    private double amount;        // Encapsulated
+    private String type;          // Income / Expense
+    private String date;
+    private String category;
+
+    public Transaction(double amount, String type, String date, String category) {
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+        this.category = category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
+
