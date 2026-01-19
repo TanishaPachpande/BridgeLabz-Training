818fd4b4a99f497003d1866fb3083400c642a3c0
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-19 13:04:02 +0530

    [Tanisha Pachpande] Add: Added CourseType.java file in universitycoursemanagement package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/javagenerics/universitycoursemanagement/CourseType.java
@@ -0,0 +1,9 @@
+package com.javagenerics.universitycoursemanagement;
+
+abstract class CourseType {
+
+    protected String evaluationMethod;
+
+    public abstract String getEvaluationMethod();
+}
+
