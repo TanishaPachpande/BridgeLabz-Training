4ab983b6d5ac44dfcafdd606f9d5cd83e4881b00
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-20 23:09:00 +0530

    [Tanisha Pachpande] Add: Added Student.java file in eduresults package

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/com/eduresults/Student.java
@@ -0,0 +1,12 @@
+package com.eduresults;
+
+class Student {
+    int score;
+    String name;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
