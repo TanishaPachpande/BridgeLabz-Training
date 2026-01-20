188da7087256bb151c91d0e40391347ca95072b6
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-20 23:11:40 +0530

    [Tanisha Pachpande] Add: Added Product.java file in flashdealz package

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/com/flashdealz/Product.java
@@ -0,0 +1,12 @@
+package com.flashdealz;
+
+class Product {
+    String name;
+    int discount;
+
+    Product(String name, int discount) {
+        this.name = name;
+        this.discount = discount;
+    }
+}
+
