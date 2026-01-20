99eb485e3d10b8857bf65b5a81b394d6d0db8640
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-20 23:13:58 +0530

    [Tanisha Pachpande] Add: Added Vehicle.java file in fleetmanager package

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/com/fleetmanager/Vehicle.java
@@ -0,0 +1,11 @@
+package com.fleetmanager;
+
+public class Vehicle {
+	int vehicleNumber;
+	int mileage;
+	
+	public Vehicle(int vehicleNumber, int mileage) {
+		this.vehicleNumber = vehicleNumber;
+		this.mileage = mileage;
+	}
+}
