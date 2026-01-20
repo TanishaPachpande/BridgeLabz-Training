391273f84c494b6780c7c56394aa0cf92005da03
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-20 23:10:18 +0530

    [Tanisha Pachpande] Add: Added User.java file in fitnesstracker package

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/com/fitnesstracker/User.java
@@ -0,0 +1,12 @@
+package com.fitnesstracker;
+
+class User {
+    String name;
+    int steps;
+
+    User(String name, int steps) {
+        this.name = name;
+        this.steps = steps;
+    }
+}
+
