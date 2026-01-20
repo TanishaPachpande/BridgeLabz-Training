e3632ee183a62d42504fc97cc437deeac709cef2
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-20 23:15:27 +0530

    [Tanisha Pachpande] Add: Added Stall.java file in foodfest package

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/com/foodfest/Stall.java
@@ -0,0 +1,11 @@
+package com.foodfest;
+
+class Stall {
+    String name;
+    int footfall;
+
+    Stall(String name, int footfall) {
+        this.name = name;
+        this.footfall = footfall;
+    }
+}
