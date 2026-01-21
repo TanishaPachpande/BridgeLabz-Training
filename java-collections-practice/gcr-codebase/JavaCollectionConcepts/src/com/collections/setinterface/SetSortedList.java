d15d0f14d53a2be46535ecbfb1f36f4b6c288582
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-21 21:59:49 +0530

    [Tanisha Pachpande] Add: Added SetSortedList.java file in setinterface package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/setinterface/SetSortedList.java
@@ -0,0 +1,16 @@
+package com.collections.setinterface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetSortedList {
+	public static void main(String[] args) {
+		Set<Integer> set = new HashSet<>();
+		set.add(5);
+		set.add(3);
+		set.add(9);
+		set.add(1);
+		
+		System.out.println(set);
+	}
+}
