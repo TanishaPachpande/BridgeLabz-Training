5029932258538254c04039f7b367a61170117a70
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-21 21:58:11 +0530

    [Tanisha Pachpande] Add: Added CompareSets.java file in setinterface package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/setinterface/CompareSets.java
@@ -0,0 +1,24 @@
+package com.collections.setinterface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CompareSets {
+    public static void main(String[] args) {
+
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(2);
+        set2.add(1);
+
+        boolean areEqual = set1.equals(set2);
+
+        System.out.println(areEqual);
+    }
+}
+
