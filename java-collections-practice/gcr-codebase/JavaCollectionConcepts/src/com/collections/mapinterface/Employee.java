4eb606fb6336468faeb2938f6e17f88c0981df15
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-21 21:49:47 +0530

    [Tanisha Pachpande] Add: Added Employee.java file in mapinterface package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/mapinterface/Employee.java
@@ -0,0 +1,12 @@
+package com.collections.mapinterface;
+
+class Employee {
+    String name;
+    String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+}
+
