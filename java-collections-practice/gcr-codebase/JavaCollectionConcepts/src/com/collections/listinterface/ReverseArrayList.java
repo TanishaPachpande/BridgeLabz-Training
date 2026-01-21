7e6a446007273070cb4c864ed2e18c6e8a2a82a5
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-21 21:46:10 +0530

    [Tanisha Pachpande] Add: Added ReverseArrayList.java file in listinterface package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/listinterface/ReverseArrayList.java
@@ -0,0 +1,31 @@
+package com.collections.listinterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseArrayList {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        int start = 0;
+        int end = list.size() - 1;
+
+        while (start < end) {
+            int temp = list.get(start);
+            list.set(start, list.get(end));
+            list.set(end, temp);
+
+            start++;
+            end--;
+        }
+
+        System.out.println(list);
+    }
+}
+
