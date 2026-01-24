a093f076d75cedc2805448ee0006aaf48f58a9e4
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-24 07:49:39 +0530

    [Tanisha Pachpande] Add: Added Employee.java file in stream package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/stream/Employee.java
@@ -0,0 +1,28 @@
+package com.stream;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+        System.out.println("-----------------------");
+    }
+}
