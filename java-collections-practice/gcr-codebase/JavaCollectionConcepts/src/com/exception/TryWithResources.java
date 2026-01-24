a7bb22fe42479334363b2b74cdf0d411fce12b36
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-24 13:31:33 +0530

    [Tanisha Pachpande] Add: Added TryWithResources.java file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/exception/TryWithResources.java
@@ -0,0 +1,22 @@
+package com.exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+
+            String firstLine = reader.readLine();
+            if (firstLine != null) {
+                System.out.println(firstLine);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
+
