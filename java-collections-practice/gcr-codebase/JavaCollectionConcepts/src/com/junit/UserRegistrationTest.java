10dc18df37586982f6a5d7369aa9c9c0ce68edb4
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 17:28:46 +0530

    [Tanisha Pachpande] Add: Added UserRegistrationTest.java file in junit package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/junit/UserRegistrationTest.java
@@ -0,0 +1,47 @@
+package com.junit;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class UserRegistrationTest {
+
+	UserRegistration userReg = new UserRegistration();
+
+    @Test
+    void testValidRegistration() {
+        assertDoesNotThrow(() -> {
+            userReg.registerUser("Tanisha", "tanisha@example.com", "Password123");
+        });
+    }
+
+    @Test
+    void testEmptyUsername() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userReg.registerUser("", "user@example.com", "Password123");
+        });
+    }
+
+    @Test
+    void testInvalidEmail() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userReg.registerUser("User", "userexample.com", "Password123");
+        });
+    }
+
+    @Test
+    void testShortPassword() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userReg.registerUser("User", "user@example.com", "pass");
+        });
+    }
+
+    @Test
+    void testNullValues() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            userReg.registerUser(null, null, null);
+        });
+    }
+
+}
