e868485f6423c3d0b9d0ed0d46a7dfadc75e0567
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 17:32:30 +0530

    [Tanisha Pachpande] Add: Added InvokePrivateMethod.java file in reflection package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/reflection/InvokePrivateMethod.java
@@ -0,0 +1,25 @@
+package com.reflection;
+
+import java.lang.reflect.Method;
+
+class Calculator {
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+public class InvokePrivateMethod {
+
+    public static void main(String[] args) throws Exception {
+
+        Calculator calculator = new Calculator();
+        Class<?> clazz = calculator.getClass();
+
+        Method method = clazz.getDeclaredMethod("multiply", int.class, int.class);
+        method.setAccessible(true);
+
+        int result = (int) method.invoke(calculator, 4, 5);
+        System.out.println("Result: " + result);
+    }
+}
+
