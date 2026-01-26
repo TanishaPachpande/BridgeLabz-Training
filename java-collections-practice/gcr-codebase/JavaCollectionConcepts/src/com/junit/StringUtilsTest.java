8fee1266b72a559e1b585f52c3d39be26e86a10a
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 17:26:36 +0530

    [Tanisha Pachpande] Add: Added StringUtilsTest.java file in junit package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/junit/StringUtilsTest.java
@@ -0,0 +1,41 @@
+package com.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StringUtilsTest {
+	StringUtils s;
+	
+	@BeforeEach
+	void setup() {
+		s = new StringUtils();
+	}
+	
+	@Test
+	void testReverse() {
+		assertEquals("olleh", s.reverse("hello"));
+	}
+	
+	@Test
+	void testisPalindromeTrue() {
+		assertTrue(s.isPalindrome("anna"));
+	}
+	
+	@Test
+	void testisPalindromeFalse() {
+		assertFalse(s.isPalindrome("anii"));
+	}
+	
+	@Test
+	void testToUppercase() {
+		assertEquals("HELLO", s.convertToUpperCase("hello"));
+	}
+	
+	@Test
+	void testNull() {
+		assertNull(s.reverse(""));
+	}
+
+}
