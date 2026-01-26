180866ceaa46081dab3ee5ad7046536c52dd12dc
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 17:31:05 +0530

    [Tanisha Pachpande] Add: Added DynamicMethodInvocation.java file in reflection package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/reflection/DynamicMethodInvocation.java
@@ -0,0 +1,45 @@
+package com.reflection;
+
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+class MathOperations {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+public class DynamicMethodInvocation {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter method name (add/subtract/multiply): ");
+        String methodName = sc.nextLine();
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        MathOperations obj = new MathOperations();
+        Class<?> clazz = obj.getClass();
+
+        Method method = clazz.getMethod(methodName, int.class, int.class);
+        int result = (int) method.invoke(obj, a, b);
+
+        System.out.println("Result: " + result);
+    }
+}
+
