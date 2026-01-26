e54d1f7e326093ea4b7a3784b0af5c8950eb00f5
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 17:25:44 +0530

    [Tanisha Pachpande] Add: Added PasswordValidator.java file in junit package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/junit/PasswordValidator.java
@@ -0,0 +1,25 @@
+package com.junit;
+
+public class PasswordValidator {
+
+    public boolean isValid(String password) {
+        if (password == null) {
+            return false;
+        }
+
+        boolean hasUpperCase = false;
+        boolean hasDigit = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                hasUpperCase = true;
+            }
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+        }
+
+        return password.length() >= 8 && hasUpperCase && hasDigit;
+    }
+}
+
