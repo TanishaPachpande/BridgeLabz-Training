2f7dda0a72d97cf3ee1a3a6c5e35fb1c152ce3e5
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 17:23:25 +0530

    [Tanisha Pachpande] Add: Added DateFormatterTest.java file in junit package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/junit/DateFormatterTest.java
@@ -0,0 +1,40 @@
+package com.junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class DateFormatterTest {
+
+	DateFormatter formatter = new DateFormatter();
+
+    @Test
+    void testValidDate() {
+        assertEquals("26-01-2026", formatter.formatDate("2026-01-26"));
+        assertEquals("01-12-2025", formatter.formatDate("2025-12-01"));
+    }
+
+    @Test
+    void testInvalidDateFormat() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            formatter.formatDate("26/01/2026");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            formatter.formatDate("2026-13-01"); // invalid month
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            formatter.formatDate("abcd-ef-gh"); // random string
+        });
+    }
+
+    @Test
+    void testNullDate() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            formatter.formatDate(null);
+        });
+    }
+
+}
