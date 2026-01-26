0ec6461bf7bbdbeee7b4aa3f95eeeb79649318aa
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 18:31:44 +0530

    [Tanisha Pachpande] Add: Added MathAnswerSheet.java file in examscanner package

---
+++ java-collections-practice/scenario-based/ScenarioBasedQuestions/src/com/examscanner/MathAnswerSheet.java
@@ -0,0 +1,10 @@
+package com.examscanner;
+
+import java.util.List;
+
+public class MathAnswerSheet extends AnswerSheet {
+    public MathAnswerSheet(List<Character> answers) {
+        super(answers);
+    }
+}
+
