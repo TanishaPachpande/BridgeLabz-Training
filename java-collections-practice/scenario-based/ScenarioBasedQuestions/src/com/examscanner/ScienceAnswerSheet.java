ab23b4656c5ab0f6488cc224500084d5a5265baf
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 18:32:02 +0530

    [Tanisha Pachpande] Add: Added ScienceAnswerSheet.java file in examscanner package

---
+++ java-collections-practice/scenario-based/ScenarioBasedQuestions/src/com/examscanner/ScienceAnswerSheet.java
@@ -0,0 +1,10 @@
+package com.examscanner;
+
+import java.util.List;
+
+public class ScienceAnswerSheet extends AnswerSheet {
+    public ScienceAnswerSheet(List<Character> answers) {
+        super(answers);
+    }
+}
+
