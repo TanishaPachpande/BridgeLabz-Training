48a6bd6a60d9e23da0ffa6bdb6893b4da0242b87
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-26 17:24:35 +0530

    [Tanisha Pachpande] Add: Added ListManagerTest.java file in junit package

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/junit/ListManagerTest.java
@@ -0,0 +1,48 @@
+package com.junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class ListManagerTest {
+
+	ListManager listManager = new ListManager();
+
+    @Test
+    void testAddElement() {
+        List<Integer> list = new ArrayList<>();
+
+        listManager.addElement(list, 10);
+
+        assertEquals(1, list.size());
+        assertTrue(list.contains(10));
+    }
+
+    @Test
+    void testRemoveElement() {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+
+        listManager.removeElement(list, 10);
+
+        assertEquals(1, list.size());
+        assertFalse(list.contains(10));
+    }
+
+    @Test
+    void testGetSize() {
+        List<Integer> list = new ArrayList<>();
+
+        listManager.addElement(list, 5);
+        listManager.addElement(list, 15);
+
+        assertEquals(2, listManager.getSize(list));
+    }
+
+}
