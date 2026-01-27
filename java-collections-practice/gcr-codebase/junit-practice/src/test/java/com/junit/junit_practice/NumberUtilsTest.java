e37c801e2e69f8281163f3d4641d297040856bfc
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-27 15:11:26 +0530

    [Tanisha Pachpande] Modify: Modified NumberUtilsTest.java file in junit_practice test package

--- java-collections-practice/gcr-codebase/junit-practice/src/test/java/com/junit/junit_practice/NumberUtilsTest.java
+++ java-collections-practice/gcr-codebase/junit-practice/src/test/java/com/junit/junit_practice/NumberUtilsTest.java
@@ -1,10 +1,12 @@
-package com.junitPractice;
+package com.junit.junit_practice;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import com.junit.junit_practice.NumberUtils;
 
 class NumberUtilsTest {
 
