001a484f670324d24a3f817f44d8721bb7a06edb
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-27 15:10:50 +0530

    [Tanisha Pachpande] Modify: Modified ListManagerTest.java file in junit_practice test package

--- java-collections-practice/gcr-codebase/junit-practice/src/test/java/com/junit/junit_practice/ListManagerTest.java
+++ java-collections-practice/gcr-codebase/junit-practice/src/test/java/com/junit/junit_practice/ListManagerTest.java
@@ -1,4 +1,4 @@
-package com.junitPractice;
+package com.junit.junit_practice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import com.junit.junit_practice.ListManager;
 
 class ListManagerTest {
 
