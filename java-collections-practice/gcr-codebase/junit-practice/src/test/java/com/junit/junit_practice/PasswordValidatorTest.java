f3901430ddf13c6498bd828330af64ecc9c4cf92
TanishaPachpande <tanishapachpande86@gmail.com>
2026-01-27 15:12:10 +0530

    [Tanisha Pachpande] Modify: Modified PasswordValidatorTest.java file in junit_practice test package

--- java-collections-practice/gcr-codebase/junit-practice/src/test/java/com/junit/junit_practice/PasswordValidatorTest.java
+++ java-collections-practice/gcr-codebase/junit-practice/src/test/java/com/junit/junit_practice/PasswordValidatorTest.java
@@ -1,9 +1,11 @@
-package com.junitPractice;
+package com.junit.junit_practice;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import com.junit.junit_practice.PasswordValidator;
 
 public class PasswordValidatorTest {
 
