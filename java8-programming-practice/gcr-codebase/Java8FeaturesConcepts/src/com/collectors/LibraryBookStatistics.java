7d954f8719bd1243c602e25bbb7a61b8a92cc870
TanishaPachpande <tanishapachpande86@gmail.com>
2026-02-03 12:41:24 +0530

    [Tanisha Pachpande] Add: Added LibraryBookStatistics.java in collectors package

---
+++ java8-programming-practice/gcr-codebase/Java8FeaturesConcepts/src/com/collectors/LibraryBookStatistics.java
@@ -0,0 +1,44 @@
+package com.collectors;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Book {
+    String genre;
+    int pages;
+
+    public Book(String genre, int pages) {
+        this.genre = genre;
+        this.pages = pages;
+    }
+}
+
+public class LibraryBookStatistics {
+    public static void main(String[] args) {
+
+        List<Book> books = List.of(
+                new Book("Fiction", 300),
+                new Book("Fiction", 450),
+                new Book("Science", 500),
+                new Book("Science", 350),
+                new Book("History", 600)
+        );
+
+        Map<String, IntSummaryStatistics> statsByGenre =
+                books.stream()
+                     .collect(Collectors.groupingBy(
+                             b -> b.genre,
+                             Collectors.summarizingInt(b -> b.pages)
+                     ));
+
+        statsByGenre.forEach((genre, stats) -> {
+            System.out.println("Genre: " + genre);
+            System.out.println("Total Pages: " + stats.getSum());
+            System.out.println("Average Pages: " + stats.getAverage());
+            System.out.println("Max Pages: " + stats.getMax());
+            System.out.println("---------------------");
+        });
+    }
+}
