c2efde782f174d9b37bdb7de9614ffb5f3932204
TanishaPachpande <tanishapachpande86@gmail.com>
2026-02-09 08:04:36 +0530

    [Tanisha Pachpande] Added User.java file

---
+++ java-design-pattern-practice/gcr-codebase/DesignPatternsAndMultithreading/studentlibrarymanagement/src/main/java/com/studentlibrarymanagement/User.java
@@ -0,0 +1,12 @@
+package com.studentlibrarymanagement;
+
+
+public abstract class User {
+	protected String name;
+
+	public User(String name) {
+		this.name = name;
+	}
+
+	public abstract void getRole();
+}
